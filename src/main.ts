8c72071a71129259ccc32439ed70aa546e29f39b
Abubakr4Dev <133663527+Abubakr4Dev@users.noreply.github.com>
2024-03-09 16:21:49 -0800

    Basic Auth Logic Done.

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+  const port: number = configService.get<number>('PORT');
+
+app.enableCors({
+  origin: [
+    'http://localhost:3000',
+    'http://example.com',
+    'http://www.example.com',
+    'http://app.example.com',
+    'https://example.com',
+    'https://www.example.com',
+    'https://app.example.com',
+  ],
+  methods: ['GET', 'POST'],
+  credentials: true,
+});
+  await app.listen(port);
+}
+bootstrap();
